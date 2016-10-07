abd6b3dbb5268b7c4a919034c35c15d42bd29101
Han Seoul-Oh <laughinghan@gmail.com>
2016-10-06 18:34:14 -0700

    Move Three.js to NPM; use its util files
    ...rather than vendoring the util files.

--- assets/scripts/entry.js
+++ assets/scripts/entry.js
@@ -2,3 +2,8 @@ window.Grapher = {};
 
 window.math = require('mathjs');
 require('babel?presets=react!./ui.js'); // mutates window.Grapher
+
+window.Detector = require('three/examples/js/Detector.js');
+window.THREE = require('three');
+require('three/examples/js/controls/TrackballControls.js'); // mutates window.THREE
+require('three/examples/js/controls/OrbitControls.js'); // mutates window.THREE
