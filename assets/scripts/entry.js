c34fe39a347ee5eeba4f75e148761f4e98103dd0
Han Seoul-Oh <laughinghan@gmail.com>
2016-10-06 19:00:59 -0700

    Reorg entry.js

--- assets/scripts/entry.js
+++ assets/scripts/entry.js
@@ -1,8 +1,4 @@
-window.Grapher = {};
-
-window.math = require('mathjs');
-require('babel?presets=react!./ui.js'); // mutates window.Grapher
-
+// dependencies
 window.Detector = require('three/examples/js/Detector.js');
 window.THREE = require('three');
 require('three/examples/js/controls/TrackballControls.js'); // mutates window.THREE
@@ -12,3 +8,9 @@ window.THREEx = {};
 require('imports?THREEx=>window.THREEx!./vendor/THREEx.KeyboardState.js'); // mutates window.THREEx
 require('imports?THREEx=>window.THREEx!./vendor/THREEx.WindowResize.js'); // mutates window.THREEx
 require('imports?THREEx=>window.THREEx!./vendor/THREEx.FullScreen.js'); // mutates window.THREEx
+
+window.math = require('mathjs');
+
+// code for our actual app
+window.Grapher = {};
+require('babel?presets=react!./ui.js'); // mutates window.Grapher
