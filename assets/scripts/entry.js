6a416237c8893af75455dfe6127ee8d6746231c5
Han Seoul-Oh <laughinghan@gmail.com>
2016-10-06 18:54:04 -0700

    Use webpack to load THREEx.*
    Also move them to vendor/

--- assets/scripts/entry.js
+++ assets/scripts/entry.js
@@ -7,3 +7,8 @@ window.Detector = require('three/examples/js/Detector.js');
 window.THREE = require('three');
 require('three/examples/js/controls/TrackballControls.js'); // mutates window.THREE
 require('three/examples/js/controls/OrbitControls.js'); // mutates window.THREE
+
+window.THREEx = {};
+require('imports?THREEx=>window.THREEx!./vendor/THREEx.KeyboardState.js'); // mutates window.THREEx
+require('imports?THREEx=>window.THREEx!./vendor/THREEx.WindowResize.js'); // mutates window.THREEx
+require('imports?THREEx=>window.THREEx!./vendor/THREEx.FullScreen.js'); // mutates window.THREEx
